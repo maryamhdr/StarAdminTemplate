b3706e21c0d5013d837a30bca66ffa56b9ee5967
MaryamHeydarian <maryamhdr76@gmail.com>
2019-08-26 18:38:49 +0430

    navbar dropdowns successfully added

---
+++ index.js
@@ -0,0 +1,12 @@
+"usestrict";
+
+var inboxDropdown = document.getElementById("inboxDropdown");
+var notifDropdown = document.getElementById("notifDropdown");
+
+function openInboxDropdown() {
+    inboxDropdown.style.display = "block"
+}
+
+function openNotifDropdown() {
+    notifDropdown.style.display = "block"
+}
