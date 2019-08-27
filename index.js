30ca5e9d72ffec224c8e9fd9b2fc32ed7d8162e6
MaryamHeydarian <maryamhdr76@gmail.com>
2019-08-27 22:16:24 +0430

    functionality added to side nav dropdowns

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var inboxDropdown = document.getElementById("inboxDropdown");
 var notifDropdown = document.getElementById("notifDropdown");
 var menu = document.getElementById("navMenu");
+var uiComponentsDropdown = document.getElementById("uiComponentsDropdown");
+var samplePagesDropdown = document.getElementById("samplePagesDropdown");
+var uiComponentsDropdownHandler = 0;
+var samplePagesDropdownHandler = 0;
 
 window.onclick = function (event) {
     inboxDropdown.style.display = "none";
@@ -25,4 +29,35 @@ function openNotifDropdown(event) {
 function openMenu(event) {
     event.stopPropagation();
     menu.style.height = "180px";
+}
+
+function handleDropdown(event, item) {
+    switch (item) {
+        case 1:
+            // console.log(1);
+            // console.log(samplePagesDropdownHandler);
+            if(samplePagesDropdownHandler === 0){
+                samplePagesDropdown.style.height = "190px";
+                samplePagesDropdownHandler = 1;
+                event.style.backgroundColor = "#f1f6ff";
+            } else {
+                samplePagesDropdown.style.height = "0";
+                samplePagesDropdownHandler = 0;
+                event.style.backgroundColor = "#ffffff";
+            }
+            break;
+        case 2:
+            // console.log(2);
+            // console.log(uiComponentsDropdownHandler);
+            if(uiComponentsDropdownHandler === 0){
+                uiComponentsDropdown.style.height = "580px";
+                uiComponentsDropdownHandler = 1;
+                event.style.backgroundColor = "#f1f6ff";
+            } else {
+                uiComponentsDropdown.style.height = "0";
+                uiComponentsDropdownHandler = 0;
+                event.style.backgroundColor = "#ffffff";
+            }
+            break;
+    }
 }
